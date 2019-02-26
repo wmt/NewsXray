ac551341dbb881aaef6969d24e0e3e6677e269da
cnaguila <38264063+cnaguila@users.noreply.github.com>
2019-02-26 11:35:09 -0800

    Update App.js

--- App.js
+++ App.js
@@ -32,6 +32,7 @@ export default class App extends React.Component {
       Asset.loadAsync([
         require('./assets/images/robot-dev.png'),
         require('./assets/images/robot-prod.png'),
+        require('./assets/images/AOC.png'),
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
@@ -39,6 +40,9 @@ export default class App extends React.Component {
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        'lato-reg': require('./assets/fonts/Lato/Lato-Regular.ttf'),
+        'lato-bold': require('./assets/fonts/Lato/Lato-Bold.ttf'),
+        
       }),
     ]);
   };
