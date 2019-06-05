ed9e9d37fc308d2a04b831d2b9fcf5579c5b137d
derricms <34073728+derricms@users.noreply.github.com>
2019-06-05 00:43:56 -0700

    Update CompressImage.js

--- backend/CompressImage.js
+++ backend/CompressImage.js
@@ -4,10 +4,22 @@ export default class ImageCompressor {
 
     static async compressImage( imageURI ) {
 
-        console.log("DEV_MSG: inside compressImage()")
-
         let imageManip = await ImageManipulator.manipulateAsync(
             imageURI, [{resize: { width: 600 } } ], { // maximum 
+              compress : 1.0, 
+              format : 'png',
+              base64 : true, }
+        ); 
+
+        return imageManip.base64; 
+
+    }
+    
+    static async compressSmallImage( imageURI ) {
+
+
+        let imageManip = await ImageManipulator.manipulateAsync(
+            imageURI, [{resize: { width: 100 } } ], { // maximum 
               compress : 1.0, 
               format : 'png',
               base64 : true, }
